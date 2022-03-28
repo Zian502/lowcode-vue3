d6dec44c5c9a210c32f7b906c6cec84ec5d4fb9f
梓安 <yuanzhian@cai-inc.com>
2022-03-28 09:29:31 +0800

    docs📝: Modify route

--- src/router/router.config.ts
+++ src/router/router.config.ts
@@ -3,14 +3,14 @@ import type { RouteRecordRaw } from 'vue-router';
 import BasicLayout from '/@/layouts/index.vue';
 
 const constantRoutes: RouteRecordRaw[] = [{
-  path: '/lowcode-vue3',
+  path: '/lowcode',
   name: 'lowcode',
   component: BasicLayout,
   meta: {
     title: '低代码'
   },
   children: [{
-    path: '/lowcode-vue3/lowcode/worksheet',
+    path: '/lowcode/worksheet',
     name: 'worksheet',
     component: () => import("/@/pages/worksheet/index.vue"),
     meta: {
