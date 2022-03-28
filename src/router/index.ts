3a468d30b53c50d4e8e1fcc89527165ed05503f0
梓安 <yuanzhian@cai-inc.com>
2022-03-28 09:27:06 +0800

    feat✨: Modify route to hash mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import routes from './router.config';
 
 // app router
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   strict: true,
   scrollBehavior: () => ({ left: 0, top: 0 }),
